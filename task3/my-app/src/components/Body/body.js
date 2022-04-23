c44281602d0a687ede6c051ed845ebc56a2ccd20
andrej558 <andrej558@yahoo.com>
2022-04-23 13:31:29 +0200

    Added body

---
+++ task3/my-app/src/components/Body/body.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import "./body.css"
+
+
+const body = (props) =>{
+
+    return(
+        <body id="body">
+        <div>
+            This is the main page.
+        </div>
+        </body>
+
+    )
+   
+    
+
+}
+
+
+
+export default body;
